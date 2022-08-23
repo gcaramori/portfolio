2aa006d71022bc371a2be5040990a2470296fd88
Guilherme <guilherme@sunsetsolutions.com.br>
2022-08-23 13:21:56 -0300

    Finished header and created animation component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { Heading, Flex, Box, Text, Link, Button } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons';
-import { Scene, PerspectiveCamera } from 'three';
-import { IconContext } from 'react-icons';
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
 
 const Header = () => {
     const { colorMode, toggleColorMode } = useColorMode();
@@ -14,20 +12,21 @@ const Header = () => {
     return (
         <header>
             <Flex justifyContent={'center'} alignItems={'center'} position='relative' px={20} py={2} bg={headerBg} boxShadow="md">
-                <Flex justifyContent={'space-between'} alignItems={'center'} mr='300px'>
-                    <Heading element={'h1'} href="#" display={'inline-block'} color={fontColor} fontSize={16} fontWeight={600} mr={16}>Guilherme Caramori</Heading>
-                    <IconContext.Provider value={{ size: "2em" }}>
-                        <Link href="https://github.com/gcaramori" display={'inline-block'} color={fontColor} mr={5} isExternal>
-                            <AiFillGithub />
-                        </Link>
-                        <Link href="https://www.linkedin.com/in/gcaramori/" display={'inline-block'} color={fontColor} isExternal>
-                            <AiFillLinkedin />
-                        </Link>
-                    </IconContext.Provider>
+                <Flex justifyContent={'space-between'} alignItems={'center'} mr={['100px', '200px', '300px']}>
+                    <Heading element={'h1'} href="#" display={'inline-block'} color={fontColor} fontSize={16} fontWeight={600} mr={14}>
+                        Guilherme Caramori
+                    </Heading>
+                    <Link href="#" display={'inline-block'} color={fontColor} fontSize={14} fontWeight={500} mr={5} isExternal>
+                        Projetos
+                    </Link>
+                    <Link href="https://github.com/gcaramori" display={'flex'} justifyContent={'center'} alignItems={'center'} color={fontColor} fontSize={14} fontWeight={500} isExternal>
+                        <AiFillGithub />
+                        <Text ml={2}>Código-fonte</Text>
+                    </Link>
                 </Flex>
                 <Box>
-                    <Button w='28px' h='35px' colorScheme='yellow' variant='solid' onClick={() => toggleColorMode()}>
-                    { colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+                    <Button w='28px' h='35px' colorScheme={ colorMode === 'dark' ? 'yellow' : 'purple' } variant='solid' onClick={() => toggleColorMode()}>
+                        { colorMode === 'dark' ? <SunIcon /> : <MoonIcon /> }
                     </Button>
                 </Box>
             </Flex>
