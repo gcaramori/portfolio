b093121b4b0d2dc9a3565b2084dad32537acf9f5
Guilherme <guilherme@sunsetsolutions.com.br>
2022-10-19 13:48:47 -0300

    Projects and Skills page done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import { FiExternalLink } from 'react-icons/fi';
 const Projects = () => {
     return(
         <Flex id='projects' h='100vh' justifyContent='flex-start' alignItems='flex-start'>
-            <Flex h='100%' w='40%' flexDirection='column' justifyContent='center' alignItems='flex-start' p={20}>
+            <Flex h='100%' w='35%' flexDirection='column' justifyContent='center' alignItems='flex-start' p={20}>
                 <motion.div
                     initial={{ opacity: 0, y: -40 }}
                     whileInView={{ opacity: 1, y: 0 }}
@@ -24,95 +24,107 @@ const Projects = () => {
                     <Text fontSize={25} color='gray.500' dropShadow='(0 10px 8px rgb(0 0 0 / 0.04))(0 4px 3px rgb(0 0 0 / 0.1))' mt={15}>Um pouco do meu trabalho.</Text>
                 </motion.div>
             </Flex>
-            <Flex h='100%' w='60%' justifyContent='center' alignItems='center' p={20}>
+            <Flex h='100%' w='65%' justifyContent='center' alignItems='center' p={20}>
                 <Flex w='100%' justifyContent='center' alignItems='center' flexWrap='wrap' gap={8}>
                     <Box w='48%' h='100%'>
-                        <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
-                            <Link href='https://criptobeer.com.br' target='_blank'>
-                                <Image fit='contain' w='100%' h='220px' src='projects/criptobeer.png' alt='project_criptobeer' />
-                            </Link>
-                            <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='10px' flexDirection='column' justifyContent='center' alignItems='center' h='200px' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
-                                <Flex mb={3} alignItems='center'>
-                                    <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Criptobeer</Text>
-                                    <FiExternalLink />
+                        <motion.div
+                            initial={{ opacity: 0, y: '-100px' }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 1 }}
+                        >    
+                            <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
+                                <Link href='https://criptobeer.com.br' target='_blank'>
+                                    <Image fit='cover' w='100%' h='250px' src='projects/criptobeer.png' alt='project_criptobeer' />
+                                </Link>
+                                <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='0' flexDirection='column' justifyContent='center' alignItems='center' h='100%' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
+                                    <Flex mb={3} alignItems='center'>
+                                        <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Criptobeer</Text>
+                                        <FiExternalLink />
+                                    </Flex>
+                                    <Text fontSize='md' fontWeight='600'>
+                                        HTML / TailwindCSS / JS
+                                    </Text>
                                 </Flex>
-                                <Text fontSize='md' fontWeight='600'>
-                                    HTML / TailwindCSS / JS
-                                </Text>
-                            </Flex>
-                        </Box>
-                        <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
-                            <Link href='https://panel.performa.ai' target='_blank'>
-                                <Image fit='contain' w='100%' h='220px' src='projects/performa.png' alt='project_performa' />
-                            </Link>
-                            <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='10px' flexDirection='column' justifyContent='center' alignItems='center' h='200px' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
-                                <Flex mb={3} alignItems='center'>
-                                    <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Performa.AI</Text>
-                                    <FiExternalLink />
+                            </Box>
+                            <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
+                                <Link href='https://panel.performa.ai' target='_blank'>
+                                    <Image fit='cover' w='100%' h='250px' src='projects/performa.png' alt='project_performa' />
+                                </Link>
+                                <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='0' flexDirection='column' justifyContent='center' alignItems='center' h='100%' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
+                                    <Flex mb={3} alignItems='center'>
+                                        <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Performa.AI</Text>
+                                        <FiExternalLink />
+                                    </Flex>
+                                    <Text fontSize='md' fontWeight='600'>
+                                        HTML / CSS / PHP
+                                    </Text>
                                 </Flex>
-                                <Text fontSize='md' fontWeight='600'>
-                                    HTML / CSS / PHP
-                                </Text>
-                            </Flex>
-                        </Box>
-                        <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
-                            <Link href='https://vetpalmas.com.br' target='_blank'>
-                                <Image fit='contain' w='100%' h='220px' src='projects/vetpalmas.png' alt='project_vetpalmas' />
-                            </Link>
-                            <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='10px' flexDirection='column' justifyContent='center' alignItems='center' h='200px' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
-                                <Flex mb={3} alignItems='center'>
-                                    <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Vetpalmas</Text>
-                                    <FiExternalLink />
+                            </Box>
+                            <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
+                                <Link href='https://vetpalmas.com.br' target='_blank'>
+                                    <Image fit='cover' w='100%' h='250px' src='projects/vetpalmas.png' alt='project_vetpalmas' />
+                                </Link>
+                                <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='0' flexDirection='column' justifyContent='center' alignItems='center' h='100%' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
+                                    <Flex mb={3} alignItems='center'>
+                                        <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Vetpalmas</Text>
+                                        <FiExternalLink />
+                                    </Flex>
+                                    <Text fontSize='md' fontWeight='600'>
+                                        HTML / TailwindCSS
+                                    </Text>
                                 </Flex>
-                                <Text fontSize='md' fontWeight='600'>
-                                    HTML / TailwindCSS
-                                </Text>
-                            </Flex>
-                        </Box>
+                            </Box>
+                        </motion.div>
                     </Box>
                     <Box w='48%' h='100%'>
-                        <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
-                            <Link href='https://paani.com.br' target='_blank'>
-                                <Image fit='contain' w='100%' h='220px' src='projects/paani.png' alt='project_paani' />
-                            </Link>
-                            <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='10px' flexDirection='column' justifyContent='center' alignItems='center' h='200px' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
-                                <Flex mb={3} alignItems='center'>
-                                    <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Paani</Text>
-                                    <FiExternalLink />
+                        <motion.div
+                            initial={{ opacity: 0, y: '-150px' }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 1.5 }}
+                        >  
+                            <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
+                                <Link href='https://paani.com.br' target='_blank'>
+                                    <Image fit='cover' w='100%' h='250px' src='projects/paani.png' alt='project_paani' />
+                                </Link>
+                                <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='0' flexDirection='column' justifyContent='center' alignItems='center' h='100%' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
+                                    <Flex mb={3} alignItems='center'>
+                                        <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Paani</Text>
+                                        <FiExternalLink />
+                                    </Flex>
+                                    <Text fontSize='md' fontWeight='600'>
+                                        HTML / CSS / PHP
+                                    </Text>
                                 </Flex>
-                                <Text fontSize='md' fontWeight='600'>
-                                    HTML / CSS / PHP
-                                </Text>
-                            </Flex>
-                        </Box>
-                        <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
-                            <Link href='https://paani.com.br' target='_blank'>
-                                <Image fit='contain' w='100%' h='220px' src='projects/criptobeer.png' alt='project_paani' />
-                            </Link>
-                            <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='10px' flexDirection='column' justifyContent='center' alignItems='center' h='200px' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
-                                <Flex mb={3} alignItems='center'>
-                                    <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Criptobeer</Text>
-                                    <FiExternalLink />
+                            </Box>
+                            <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
+                                <Link href='https://paani.com.br' target='_blank'>
+                                    <Image fit='cover' w='100%' h='250px' src='projects/criptobeer.png' alt='project_paani' />
+                                </Link>
+                                <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='0' flexDirection='column' justifyContent='center' alignItems='center' h='100%' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
+                                    <Flex mb={3} alignItems='center'>
+                                        <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Criptobeer</Text>
+                                        <FiExternalLink />
+                                    </Flex>
+                                    <Text fontSize='md' fontWeight='600'>
+                                        HTML / CSS / PHP
+                                    </Text>
                                 </Flex>
-                                <Text fontSize='md' fontWeight='600'>
-                                    HTML / CSS / PHP
-                                </Text>
-                            </Flex>
-                        </Box>
-                        <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
-                            <Link href='https://paani.com.br' target='_blank'>
-                                <Image fit='contain' w='100%' h='220px' src='projects/paani.png' alt='project_paani' />
-                            </Link>
-                            <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='10px' flexDirection='column' justifyContent='center' alignItems='center' h='200px' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
-                                <Flex mb={3} alignItems='center'>
-                                    <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Paani</Text>
-                                    <FiExternalLink />
+                            </Box>
+                            <Box position='relative' w='100%' h='30%' mb={5} borderRadius='sm' boxShadow='lg' overflow='hidden' transition='all 200ms ease-in-out' cursor='pointer' _hover={{ transform: 'scale(1.1)' }} role='group'>
+                                <Link href='https://paani.com.br' target='_blank'>
+                                    <Image fit='cover' w='100%' h='250px' src='projects/paani.png' alt='project_paani' />
+                                </Link>
+                                <Flex _groupHover={{ opacity: '1' }} opacity='0' transition='all 200ms ease-in-out' position='absolute' top='0' flexDirection='column' justifyContent='center' alignItems='center' h='100%' w='100%' p={2} bg='rgba(0, 0, 0, .6)'>
+                                    <Flex mb={3} alignItems='center'>
+                                        <Text fontSize='xl' textTransform='uppercase' fontWeight='600' mr={4}>Paani</Text>
+                                        <FiExternalLink />
+                                    </Flex>
+                                    <Text fontSize='md' fontWeight='600'>
+                                        HTML / CSS / PHP
+                                    </Text>
                                 </Flex>
-                                <Text fontSize='md' fontWeight='600'>
-                                    HTML / CSS / PHP
-                                </Text>
-                            </Flex>
-                        </Box>
+                            </Box>
+                        </motion.div>
                     </Box>
                 </Flex>
             </Flex>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Heading, Text, Flex, Box, Image } from '@chakra-ui/react';
+import { Heading, Text, Flex, Image, Box } from '@chakra-ui/react';
 import { motion } from "framer-motion";
 
 const Skills = () => {
     return(
-        <Flex id='skills' h='100vh' justifyContent='flex-start' alignItems='flex-start'>
+        <Flex id='skills' h='100vh' w='100%' justifyContent='flex-start' alignItems='flex-start' overflow='hidden'>
             <Flex h='100%' w='40%' flexDirection='column' justifyContent='center' alignItems='flex-start' p={20}>
                 <motion.div
                     initial={{ opacity: 0, y: -40 }}
@@ -24,56 +24,70 @@ const Skills = () => {
                 </motion.div>
             </Flex>
             <Flex h='100%' w='60%' justifyContent='center' alignItems='center' p={20}>
-                <Box display='block' id="skillLogos" h='100%' w='100%' position='relative'>
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0, y: '10%' }}
-                        whileInView={{ opacity: 1, scale: 1 }}   
-                        transition={{ duration: 1 }}
-                    >    
-                        <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript_logo" />
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        whileInView={{ opacity: 1, scale: 1, y: '-150px', x: '45%' }}   
-                        transition={{ duration: 1 }}
-                    >    
-                        <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html_logo" />
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        whileInView={{ opacity: 1, scale: 1, y: 0, x: '80%' }}   
-                        transition={{ duration: 1 }}
-                    >    
-                        <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="css_logo" />
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        whileInView={{ opacity: 1, scale: 1, y: '-80px' }}
-                        transition={{ duration: 1 }}
-                    >    
-                        <Image h='200px' w='200px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="tailwind_logo" />
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        whileInView={{ opacity: 1, scale: 1, y: '-70px', x: '65%' }}   
-                        transition={{ duration: 1 }}
-                    >    
-                        <Image h='150px' w='150px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs_logo" />
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        whileInView={{ opacity: 1, scale: 1, y: '-200px', x: '10%' }}   
-                        transition={{ duration: 1 }}
-                    >    
-                        <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react_logo" />
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        whileInView={{ opacity: 1, scale: 1 }}   
-                        transition={{ duration: 1 }}
-                    >    
-                        <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt="mysql_logo" />
-                    </motion.div>
+                <Flex justifyContent='center' alignItems='center' id="skillLogos" h='100%' w='100%' position='relative'>
+                    <Box w='110px' position='absolute' top={20} left={32}>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            whileInView={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 1 }}
+                        >
+                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript_logo" />
+                        </motion.div>
+                    </Box>
+                    <Box w='110px' position='absolute' top={4} insetX={0} m='auto'>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            whileInView={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 1 }}
+                        >    
+                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html_logo" />
+                        </motion.div>
+                    </Box>
+                    <Box w='110px' position='absolute' top={20} right={32}>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            whileInView={{ opacity: 1, scale: 1 }}   
+                            transition={{ duration: 1 }}
+                        >    
+                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="css_logo" />
+                        </motion.div>
+                    </Box>
+                    <Box w='200px' position='absolute' top={72} left={5}>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            whileInView={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 1 }}
+                        >    
+                            <Image h='200px' w='200px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="tailwind_logo" />
+                        </motion.div>
+                    </Box>
+                    <Box w='200px' position='absolute' bottom={16} left={32}>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            whileInView={{ opacity: 1, scale: 1 }}   
+                            transition={{ duration: 1 }}
+                        >    
+                            <Image h='150px' w='150px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs_logo" />
+                        </motion.div>
+                    </Box>
+                    <Box w='110px' position='absolute' bottom={0} insetX={0} m='auto'>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            whileInView={{ opacity: 1, scale: 1 }}   
+                            transition={{ duration: 1 }}
+                        >    
+                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react_logo" />
+                        </motion.div>
+                    </Box>
+                    <Box w='110px' position='absolute' bottom={20} right={32}>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            whileInView={{ opacity: 1, scale: 1 }}   
+                            transition={{ duration: 1 }}
+                        >    
+                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt="mysql_logo" />
+                        </motion.div>
+                    </Box>
                     <motion.div
                         initial={{ opacity: 0, scale: 0 }}
                         whileInView={{ opacity: 1, scale: 1 }}   
@@ -81,7 +95,16 @@ const Skills = () => {
                     >    
                         <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="mongo_logo" />
                     </motion.div>
-                </Box>
+                    <Box w='110px' position='absolute' right={5}>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            whileInView={{ opacity: 1, scale: 1 }}   
+                            transition={{ duration: 1 }}
+                        >   
+                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg" alt="docker_logo" />
+                        </motion.div>
+                    </Box>
+                </Flex>
             </Flex>
         </Flex>
     );
