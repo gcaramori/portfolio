46c3666abef41be6779254f6e821396839fb8626
Guilherme Caramori <gcaramori16@gmail.com>
2022-10-23 21:54:13 -0300

    Mobile version done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,88 +20,91 @@ const Skills = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ duration: 1 }}
                 >
-                    <Text fontSize={25} color='gray.500' dropShadow='(0 10px 8px rgb(0 0 0 / 0.04))(0 4px 3px rgb(0 0 0 / 0.1))'mt={{ base: 3, lg: 15}}>As ferramentas que domino.</Text>
+                    <Text fontSize={25} color='gray.500' dropShadow='(0 10px 8px rgb(0 0 0 / 0.04))(0 4px 3px rgb(0 0 0 / 0.1))' mt={{ base: 3, lg: 15}}>As ferramentas que domino.</Text>
                 </motion.div>
             </Flex>
             <Flex h='100%' w={{ base: '100%', lg: '60%' }} justifyContent='center' alignItems='center' p={{ base: 6, lg: 20 }}>
-                <Flex justifyContent='center' alignItems='center' id="skillLogos" h='100%' w='100%' position='relative'>
-                    <Box w='110px' position='absolute' top={20} left={32}>
+                <Flex justifyContent={{ base: 'space-between', lg: 'center' }} alignItems='center' id="skillLogos" h='100%' w='100%' position='relative' flexWrap={{ base: 'wrap', lg: 'nowrap' }}>
+                    <Box w={{ base: '48%', lg: '110px' }} mb={{ base: 6, lg: 0 }} position={{ base: 'relative', lg: 'absolute' }} top={{ base: 0, lg: 20 }} left={{ base: 0, lg: 32 }}>
                         <motion.div
                             initial={{ opacity: 0, scale: 0 }}
                             whileInView={{ opacity: 1, scale: 1 }}   
                             transition={{ duration: 1 }}
                         >    
-                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="mongo_logo" />
+                            <Image h={{ base: '100%', lg: '100px' }} w={{ base: '100%', lg: '100px' }} fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="mongo_logo" />
                         </motion.div>
                     </Box>
-                    <Box w='110px' position='absolute' top={4} insetX={0} m='auto'>
+                    <Box w={{ base: '48%', lg: '110px' }} mb={{ base: 6, lg: 0 }} position={{ base: 'relative', lg: 'absolute' }} top={{ base: 0, lg: 4 }} insetX={0} m={{ base: 'unset', lg: 'auto' }}>
                         <motion.div
                             initial={{ opacity: 0, scale: 0 }}
                             whileInView={{ opacity: 1, scale: 1 }}
                             transition={{ duration: 1 }}
                         >    
-                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html_logo" />
+                            <Image h={{ base: '100%', lg: '100px' }} w={{ base: '100%', lg: '100px' }} fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html_logo" />
                         </motion.div>
                     </Box>
-                    <Box w='110px' position='absolute' top={20} right={32}>
+                    <Box w={{ base: '48%', lg: '110px' }} mb={{ base: 6, lg: 0 }} position={{ base: 'relative', lg: 'absolute' }} top={{ base: 0, lg: 20 }} right={{ base: 0, lg: 32 }}>
                         <motion.div
                             initial={{ opacity: 0, scale: 0 }}
                             whileInView={{ opacity: 1, scale: 1 }}   
                             transition={{ duration: 1 }}
                         >    
-                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="css_logo" />
+                            <Image h={{ base: '100%', lg: '100px' }} w={{ base: '100%', lg: '100px' }} fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="css_logo" />
                         </motion.div>
                     </Box>
-                    <Box w='110px' position='absolute' left={5}>
+                    <Box w={{ base: '48%', lg: '110px' }} mb={{ base: 6, lg: 0 }} position={{ base: 'relative', lg: 'absolute' }} left={{ base: 0, lg: 5 }}>
                         <motion.div
                             initial={{ opacity: 0, scale: 0 }}
                             whileInView={{ opacity: 1, scale: 1 }}   
                             transition={{ duration: 1 }}
                         >    
-                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" alt="mysql_logo" />
+                            <Image h={{ base: '100%', lg: '100px' }} w={{ base: '100%', lg: '100px' }} fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" alt="mysql_logo" />
                         </motion.div>
                     </Box>
-                    <Box w='200px' position='absolute' bottom={16} left={32}>
+                    <Box w={{ base: '48%', lg: '200px' }} mb={{ base: 6, lg: 0 }} position={{ base: 'relative', lg: 'absolute' }} bottom={{ base: 0, lg: 16 }} left={{ base: 0, lg: 32 }}>
                         <motion.div
                             initial={{ opacity: 0, scale: 0 }}
                             whileInView={{ opacity: 1, scale: 1 }}   
                             transition={{ duration: 1 }}
                         >    
-                            <Image h='150px' w='150px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs_logo" />
+                            <Image h={{ base: '100%', lg: '150px' }} w={{ base: '100%', lg: '150px' }} fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs_logo" />
                         </motion.div>
                     </Box>
-                    <Box w='110px' position='absolute' bottom='-10%' insetX={0} m='auto'>
+                    <Box w={{ base: '48%', lg: '110px' }} mb={{ base: 6, lg: 0 }} position={{ base: 'relative', lg: 'absolute' }} bottom={{ base: 0, lg: '-10%' }} insetX={0} m={{ base: 'unset', lg: 'auto' }}>
                         <motion.div
                             initial={{ opacity: 0, scale: 0 }}
                             whileInView={{ opacity: 1, scale: 1 }}
                             transition={{ duration: 1 }}
                         >    
-                            <Image h='200px' w='200px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg" alt="php_logo" />
+                            <Image h={{ base: '100%', lg: '200px' }} w={{ base: '100%', lg: '200px' }} fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg" alt="php_logo" />
                         </motion.div>
                     </Box>
-                    <Box w='110px' position='absolute' bottom={20} right={32}>
+                    <Box w={{ base: '48%', lg: '110px' }} mb={{ base: 6, lg: 0 }} position={{ base: 'relative', lg: 'absolute' }} bottom={{ base: 0, lg: 20 }} right={{ base: 0, lg: 32 }}>
                         <motion.div
                             initial={{ opacity: 0, scale: 0 }}
                             whileInView={{ opacity: 1, scale: 1 }}   
                             transition={{ duration: 1 }}
                         >   
-                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg" alt="docker_logo" />
+                            <Image h={{ base: '100%', lg: '100px' }} w={{ base: '100%', lg: '100px' }} fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg" alt="docker_logo" />
                         </motion.div>
                     </Box>
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        whileInView={{ opacity: 1, scale: 1 }}
-                        transition={{ duration: 1 }}
-                    >
-                        <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript_logo" />
-                    </motion.div>
-                    <Box w='110px' position='absolute' right={5}>
+                    <Box w={{ base: '48%', lg: '110px' }} mb={{ base: 6, lg: 0 }} position='relative'>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            whileInView={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 1 }}
+                            width={{ base: '50%', lg: '110px' }}
+                        >
+                            <Image h={{ base: '100%', lg: '100px' }} w={{ base: '100%', lg: '100px' }} fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript_logo" />
+                        </motion.div>
+                    </Box>
+                    <Box w={{ base: '48%', lg: '110px' }} mb={{ base: 6, lg: 0 }} position={{ base: 'relative', lg: 'absolute' }} right={{ base: 0, lg: 5 }}>
                         <motion.div
                             initial={{ opacity: 0, scale: 0 }}
                             whileInView={{ opacity: 1, scale: 1 }}   
                             transition={{ duration: 1 }}
                         >    
-                            <Image h='100px' w='100px' fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react_logo" />
+                            <Image h={{ base: '100%', lg: '100px' }} w={{ base: '100%', lg: '100px' }} fit='contain' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react_logo" />
                         </motion.div>
                     </Box>
                 </Flex>
