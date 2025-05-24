c7f4f6e18e3ae52d71e95185cb852913eed680ea
Guilherme Caramori <gcaramori16@gmail.com>
2025-05-23 23:17:10 -0300

    fixing 3d animation: moving from root layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import dynamic from 'next/dynamic'
 import { Index } from '@/components/index'
 
+const Computer = dynamic(() =>
+  import('../components/index/computer').then((mod) => mod.Computer)
+)
+
 export default function IndexPage() {
-  return <Index />
+  return (
+    <>
+      <Index />
+      <Computer />
+    </>
+  )
 }
