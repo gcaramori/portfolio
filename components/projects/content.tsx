975bba4f6694f3687aed9fc6d66194934dfcf55b
Guilherme Caramori <gcaramori16@gmail.com>
2025-06-29 15:51:08 -0300

    working on...

--- components/projects/content.tsx
+++ components/projects/content.tsx
@@ -5,6 +5,10 @@ import { useContext } from 'react'
 import { LanguageContext } from '@/contexts/languageContext'
 import Languages from '@/lib/languages'
 import FadeInBottom from '../fadeInBottom'
+import jsbjjkylemore from '../../public/projects/kylemore.png'
+import criptobeer from '../../public/projects/criptobeer.png'
+import unit5 from '../../public/projects/unit5.png'
+import performapanel from '../../public/projects/performa.png'
 
 export function ProjectsContent() {
   const { language } = useContext(LanguageContext)
@@ -12,48 +16,64 @@ export function ProjectsContent() {
   return (
     <div className="container flex h-full flex-col justify-start relative py-6">
       <FadeInBottom>
-        <p className="block relative text-gray-500 font-inter drop-shadow-sm text-md xl:text-lg 2xl:text-2xl mb-8">
+        <p className="block relative text-[var(--primary-bg)] font-inter drop-shadow-sm mb-8">
           {Languages[language]?.projects?.text1}
         </p>
 
         <div className="flex flex-wrap justify-between items-start gap-10 relative">
           <div className="flex flex-col justify-start items-start relative">
             <Image
-              src="/projects/kylemore.png"
+              src={jsbjjkylemore}
               alt="jsbjjkylemore"
               width={300}
               height={300}
-              priority={true}
+              placeholder="blur"
+              style={{
+                maxWidth: '100%',
+                height: 'auto',
+              }}
             />
           </div>
 
           <div className="flex flex-col justify-start items-start relative">
             <Image
-              src="/projects/criptobeer.png"
+              src={criptobeer}
               alt="criptobeer"
               width={300}
               height={300}
-              priority={true}
+              placeholder="blur"
+              style={{
+                maxWidth: '100%',
+                height: 'auto',
+              }}
             />
           </div>
 
           <div className="flex flex-col justify-start items-start relative">
             <Image
-              src="/projects/unit5.png"
+              src={unit5}
               alt="unit5gym"
               width={300}
               height={300}
-              priority={true}
+              placeholder="blur"
+              style={{
+                maxWidth: '100%',
+                height: 'auto',
+              }}
             />
           </div>
 
           <div className="flex flex-col justify-start items-start relative">
             <Image
-              src="/projects/performa.png"
+              src={performapanel}
               alt="performapanel"
               width={300}
               height={300}
-              priority={true}
+              placeholder="blur"
+              style={{
+                maxWidth: '100%',
+                height: 'auto',
+              }}
             />
           </div>
         </div>
